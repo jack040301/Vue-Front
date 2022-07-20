fe36d8ccf6b2fe3a372981ec8038c3978e16d58b
Jackdaisuki04@gmail.com <jackdaisuki04@gmail.com>
2022-07-20 12:40:24 +0800

    Name fix and divider
    text font style is still working

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,55 +1,43 @@
 <script setup>
+import imagePath from "@/assets/7153683.jpg";
 
-import imagePath from "@/assets/7153683.jpg"
-
-import Header from './Header.vue'
+import Header from "./Header.vue";
 import Footer from "./Footer.vue";
-
 </script>
 
 <template>
-<div   :style="{ backgroundImage: `url(${imagePath})` }">
-
+  <div :style="{ backgroundImage: `url(${imagePath})` }">
     <Header class="bg-opacity-20"> </Header>
-<div class="hero min-h-screen">
-
-
-  <div class="hero-overlay bg-opacity-0"></div>
-  <div class="hero-content text-center text-neutral-content">
-   
-<div class="flex flex-col w-full">
-  <div class="grid place-items-center">
-
-    JACQUELINE PORRAL
-  </div> 
-  <div class="divider "></div> 
-  <div class="grid place-items-center">A BACKEND DEVELOPER AND PASSIONATE LEADER</div>
-</div>
-   
+    <div class="hero min-h-screen">
+      <div class="hero-overlay bg-opacity-0"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="flex flex-col w-full">
+          <div class="grid place-items-center">
+            <p class="text-6xl">JACQUELINE PORRAL</p>
+          </div>
+          <div
+            class="divider divider-vertical before:bg-yellow-500 after:bg-yellow-500"
+          ></div>
+          <div class="grid place-items-center">
+            <p class="text-2xl">
+              AN ASPIRING BACKEND DEVELOPER AND PASSIONATE LEADER
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-
-<Footer > </Footer>
-</div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
 
 <script>
-
-
 export default {
-  components : {
-
+  components: {},
+  data() {
+    return {
+      backimg: "@/assets/7153683.jpg",
+    };
   },
-  data(){
-   return {
-      backimg : "@/assets/7153683.jpg",
-     
-   }
-  }
-}
+};
 </script>
